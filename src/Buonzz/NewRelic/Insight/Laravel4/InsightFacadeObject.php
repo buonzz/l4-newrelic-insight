d34b25a36fce6e01f0af2068357dad6ce120cd6b
buonzz <buonzz@gmail.com>
2014-09-14 12:07:48 +0800

    fixed access to private property

--- src/Buonzz/NewRelic/Insight/Laravel4/InsightFacadeObject.php
+++ src/Buonzz/NewRelic/Insight/Laravel4/InsightFacadeObject.php
@@ -10,15 +10,15 @@ class InsightFacadeObject{
 	}
 
 	public function setQueryKey($api_key){
-		return $this->insight->query_key = $api_key;
+		return $this->insight->setQueryKey($api_key);
 	}
 
 	public function setInsertKey($api_key){
-		return $this->insight->insert_key = $api_key;
+		return $this->insight->setInsertKey($api_key);
 	}
 
 	public function setAccountID($account_id){
-		return $this->insight->account_id = $account_id;		
+		return $this->insight->setAccountID($account_id);		
 	}
 
 	public function query($NRQL){
