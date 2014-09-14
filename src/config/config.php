7138dda41b0914a459ace14491676dca71370261
buonzz <buonzz@gmail.com>
2014-09-14 12:39:23 +0800

    fixed trailing preiod

--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,6 @@ return array(
 	'account_id' => '',		
 	'query_key' => '',
 	'insert_key' => '',
-	'user_agent' => 'Buonzz Laravel Insight Library v1.0'.
+	'user_agent' => 'Buonzz Laravel Insight Library v1.0',
 	'timeout' => 15
 );
